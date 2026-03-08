2e7865bcc95ca9ad9f4371b71ff59ea106b3372b
William Cruz Hernández <wcruzhernandez59@gmail.com>
2026-03-07 23:54:15 -0600

    fix: se modificaron pantallas MainMenuScreen.kt y SplashScreen.kt, asi como se agregaron iconos de app

--- app/src/main/java/com/irrigacioninteligente/irrigacionydeteccion/ui/screens/MainMenuScreen.kt
+++ app/src/main/java/com/irrigacioninteligente/irrigacionydeteccion/ui/screens/MainMenuScreen.kt
@@ -1,5 +1,6 @@
 package com.irrigacioninteligente.irrigacionydeteccion.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,15 +8,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.irrigacioninteligente.irrigacionydeteccion.R
 import com.irrigacioninteligente.irrigacionydeteccion.ui.components.CustomButton
 import com.irrigacioninteligente.irrigacionydeteccion.ui.components.IrrigationAppBar
-import com.irrigacioninteligente.irrigacionydeteccion.ui.components.PlantIllustration
 import com.irrigacioninteligente.irrigacionydeteccion.utils.Constants
 
 @Composable
@@ -39,8 +43,12 @@ fun MainMenuScreen(
         ) {
             Spacer(modifier = Modifier.height(30.dp))
 
-            // Ilustración de la planta
-            PlantIllustration()
+            Image(
+                painter = painterResource(id = R.drawable.aloe_vera_icon),
+                contentDescription = "Aloe Vera Icon",
+                modifier = Modifier.size(180.dp),
+                contentScale = ContentScale.Fit
+            )
 
             Spacer(modifier = Modifier.height(60.dp))
 
--- app/src/main/java/com/irrigacioninteligente/irrigacionydeteccion/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/irrigacioninteligente/irrigacionydeteccion/ui/screens/SplashScreen.kt
@@ -6,16 +6,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.Image
+import com.irrigacioninteligente.irrigacionydeteccion.R
 import com.irrigacioninteligente.irrigacionydeteccion.ui.components.CustomButton
-import com.irrigacioninteligente.irrigacionydeteccion.ui.components.PlantIllustration
 import com.irrigacioninteligente.irrigacionydeteccion.utils.Constants
 
 @Composable
@@ -39,8 +43,12 @@ fun SplashScreen(onConnectClick: () -> Unit) {
 
         Spacer(modifier = Modifier.height(60.dp))
 
-        // Ilustración de la planta
-        PlantIllustration()
+        Image(
+            painter = painterResource(id = R.drawable.aloe_vera_icon),
+            contentDescription = "Aloe Vera Icon",
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Fit
+        )
 
         Spacer(modifier = Modifier.height(80.dp))
 
